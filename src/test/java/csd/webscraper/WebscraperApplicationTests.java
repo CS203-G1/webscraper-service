0832b623b47f207b13d6b2aaed57e1b998ff1a1c
Jerald <jeraldssw@gmail.com>
2021-09-26 00:18:52 +0800

    Initial commit

---
+++ src/test/java/csd/webscraper/WebscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package csd.webscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
