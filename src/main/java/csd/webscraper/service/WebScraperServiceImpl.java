6ddcd1a7562af2c19431784a29924189f3201bcb
Felice <felicepng2010@gmail.com>
2021-10-07 16:44:53 +0800

    WIP: Add GOV data

--- src/main/java/csd/webscraper/service/WebScraperServiceImpl.java
+++ src/main/java/csd/webscraper/service/WebScraperServiceImpl.java
@@ -33,14 +33,25 @@ public class WebScraperServiceImpl implements WebScraperService {
 
     @Scheduled(cron = "@midnight")
     public void scrapeData() {
-        LOGGER.info("------ STARTING TO SCRAPE " + mohUrl);
-
+        CovidData covidData = new CovidData();
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(WebScraperUtils.getChromeOptions());
 
+        scrapeMohData(covidData, driver);
+        scrapeGovData(covidData, driver);
+
+        // covidDataRepository.save(covidData);
+        System.out.println(covidData);
+
+        LOGGER.info("------ SHUTTING DOWN SELENIUM");
+        driver.quit();
+    }
+
+    public void scrapeMohData(CovidData covidData, WebDriver driver) {
+        LOGGER.info("------ STARTING TO SCRAPE " + mohUrl);
+
         driver.get(mohUrl);
         List<WebElement> elements = driver.findElements(By.className("sfContentBlock"));
-        CovidData covidData = new CovidData();
 
         for (int i = 0; i < elements.size(); i++) {
             try {
@@ -59,9 +70,45 @@ public class WebScraperServiceImpl implements WebScraperService {
                 // Consume error for data that we do not wish to store
             }
         }
-        covidDataRepository.save(covidData);
-        LOGGER.info("------ SUCCESSFULLY SCRAPED DATA");
-        LOGGER.info("------ SHUTTING DOWN SELENIUM");
-        driver.quit();
+
+        LOGGER.info("------ SUCCESSFULLY SCRAPED " + mohUrl);
+    }
+
+
+    public void scrapeGovData(CovidData covidData, WebDriver driver) {
+        LOGGER.info("------ STARTING TO SCRAPE " + govUrl);
+
+        driver.get(govUrl);
+
+        WebElement vaccineData = driver.findElement(By.id("vaccinedata"));
+        WebElement vaccineDataSibling = vaccineData.findElement(By.xpath("following-sibling::*"));
+        List<WebElement> vaccineDataElements = vaccineDataSibling.findElements(By.tagName("td"));
+
+        WebElement caseSummary = driver.findElement(By.id("casesummary"));
+        WebElement caseSummarySibling = caseSummary.findElement(By.xpath("following-sibling::*"));
+        List<WebElement> caseSummaryElements = caseSummarySibling.findElements(By.tagName("td"));
+
+        // System.out.println(vaccineDataElements.get(0).getText());
+        // System.out.println(vaccineDataElements.get(2).getText());
+        // System.out.println(vaccineDataElements.get(1).getText());
+        // System.out.println(vaccineDataElements.get(3).getText().split("\n")[0]);
+        // System.out.println(vaccineDataElements.get(4).getText());
+        // System.out.println(vaccineDataElements.get(5).getText().split("\n")[0]);
+
+        try {
+            System.out.println(vaccineDataElements.get(2).getText() + " | " + vaccineDataElements.get(3).getText().split("\r\n|\s")[0]);
+            WebScraperUtils.updateModel(covidData, vaccineDataElements.get(0).getText(), Integer.parseInt(vaccineDataElements.get(2).getText()));
+            WebScraperUtils.updateModel(covidData, vaccineDataElements.get(1).getText(), Integer.parseInt(vaccineDataElements.get(3).getText().split("\r\n")[0]));
+            WebScraperUtils.updateModel(covidData, vaccineDataElements.get(4).getText(), Integer.parseInt(vaccineDataElements.get(5).getText().split("\r\n")[0]));
+            WebScraperUtils.updateModel(covidData, caseSummaryElements.get(0).getText(), Integer.parseInt(caseSummaryElements.get(2).getText()));
+            WebScraperUtils.updateModel(covidData, caseSummaryElements.get(1).getText(), Integer.parseInt(caseSummaryElements.get(3).getText()));
+            WebScraperUtils.updateModel(covidData, caseSummaryElements.get(4).getText(), Integer.parseInt(caseSummaryElements.get(5).getText()));
+        }  catch (IndexOutOfBoundsException e) {
+            // Consume error for data that we do not wish to store
+        } catch (NumberFormatException e) {
+            // Consume error for data that we do not wish to store
+        }
+
+        LOGGER.info("------ SUCCESSFULLY SCRAPED " + govUrl);
     }
 }
