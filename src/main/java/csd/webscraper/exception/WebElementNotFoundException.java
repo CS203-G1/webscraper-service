89c08f3b997c2c38808ac24c86725dcd44124880
jeraldlyh <jeraldssw@gmail.com>
2021-10-11 17:05:59 +0800

    Update exception message in WebElementNotFoundMessage

--- src/main/java/csd/webscraper/exception/WebElementNotFoundException.java
+++ src/main/java/csd/webscraper/exception/WebElementNotFoundException.java
@@ -4,6 +4,6 @@ public class WebElementNotFoundException extends ResourceNotFoundException {
     private static final long serialVersionUID = 1L;
 
     public WebElementNotFoundException(String reason) {
-        super(reason);
+        super(String.format("Header - [%s] has been changed in the website", reason));
     }
 }
